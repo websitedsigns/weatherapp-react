703bc5fb3eae1382c6ed2a01d220d7b93499c213
websitedsigns <websitedsigns@gmail.com>
2023-09-01 13:50:06 +0100

    Error Handling

--- weatherapp-react/src/App.js
+++ weatherapp-react/src/App.js
@@ -3,9 +3,13 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [error, setError] = useState(null);
   const [weatherData, setWeatherData] = useState(null);
   const [city, setCity] = useState('');
   const apiKey = '3ec4a7cf1f19e18756476b82a0860caf';
+
+
 
   const fetchWeatherData = async () => {
     try {
@@ -13,8 +17,10 @@ function App() {
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
       );
       setWeatherData(response.data);
+      setError(null); // Clear any previous errors
     } catch (error) {
       console.error('Error fetching weather data:', error);
+      setError('Error Fetching Weather Data. Please Check The City Name ');
     }
   };
 
@@ -32,11 +38,12 @@ function App() {
         onChange={(e) => setCity(e.target.value)}
       />
       <button onClick={fetchWeatherData}>Get Weather</button>
-      {weatherData && (
-        <div>
-          <h2>{weatherData.name}</h2>
-          <p>Temperature: {weatherData.main.temp}°C</p>
-          <p>Weather: {weatherData.weather[0].main}</p>
+    {error && <p className="error-message">{error}</p>} {/* Display error message here */}
+    {weatherData && (
+      <div>
+        <h2>{weatherData.name}</h2>
+        <p>Temperature: {weatherData.main.temp}°C</p>
+        <p>Weather: {weatherData.weather[0].main}</p>
         </div>
       )}
     </div>
